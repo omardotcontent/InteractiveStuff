74653d965231e7fde7f63a07533ac30be44b3c58
Omar Mohamed <omar@merakistudios.dev>
2025-12-02 15:57:00 +0200

    Refactor hand swing sound logic for bell item in ClientPlayerEntityMixin

--- src/main/java/omar/projects/interactivestuff/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/omar/projects/interactivestuff/mixin/ClientPlayerEntityMixin.java
@@ -1,11 +1,12 @@
 package omar.projects.interactivestuff.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
-import net.minecraft.util.hit.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,15 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public final class ClientPlayerEntityMixin {
 
+    @Unique
+    private final MinecraftClient client = MinecraftClient.getInstance();
+
+
     @Inject(method = "swingHand", at = @At("HEAD"))
     private void onSwingHand(final Hand hand, final CallbackInfo ci) {
-        final ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
-
-        var client = net.minecraft.client.MinecraftClient.getInstance();
-        if (client.crosshairTarget != null && client.crosshairTarget.getType() == HitResult.Type.MISS) {
-            if (player.getStackInHand(hand).isOf(Items.BELL)) {
-                player.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.0f);
-            }
+        if (client.player.getStackInHand(hand).isOf(Items.BELL)) {
+            client.player.playSound(SoundEvents.BLOCK_BELL_USE, 1.0f, 1.0f);
         }
     }
 }
