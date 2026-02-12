74b2526ed2bee957fceb2dda5ce210500c0d50af
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-12 14:45:12 +0200

    Make GetDelta class final to prevent subclassing

--- src/main/java/omar/projects/interactivestuff/scripts/functions/GetDelta.java
+++ src/main/java/omar/projects/interactivestuff/scripts/functions/GetDelta.java
@@ -4,7 +4,7 @@ import me.abdelaziz.runtime.function.nat.NativeFunction;
 import me.abdelaziz.runtime.Value;
 import omar.projects.interactivestuff.scripts.Utilities.RenderTickHandler;
 
-public class GetDelta extends NativeFunction {
+public final class GetDelta extends NativeFunction {
 
     public GetDelta() {
         super((env, args) -> new Value(RenderTickHandler.normalizedDelta));
