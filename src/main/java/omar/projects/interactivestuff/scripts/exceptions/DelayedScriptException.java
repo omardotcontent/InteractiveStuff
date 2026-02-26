010a565ba252219e67af608c1561256571ee1c23
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-26 22:24:11 +0200

    removal/change places of classes and scripts

--- src/main/java/omar/projects/interactivestuff/scripts/exceptions/DelayedScriptException.java
+++
@@ -1,7 +0,0 @@
-package omar.projects.interactivestuff.scripts.exceptions;
-
-public class DelayedScriptException extends RuntimeException {
-    public DelayedScriptException(String message) {
-        super(message);
-    }
-}
